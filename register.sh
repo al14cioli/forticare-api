2f44036e18e8ead00a5c9adf368932fe59f0cd61
Alain Forcioli <alain.forcioli@gmail.com>
2021-02-10 22:26:10 +0100

    Modified the execution method of the python script. It is now the argument of the python command.

--- register.sh
+++ register.sh
@@ -1,33 +1,29 @@
 #! /bin/bash
 
-
-
-usage() { 
+usage() {
     echo "$0 -f <REGISTRATIONCODE.csv> -d"
     exit 0
 }
 
 [ $# -eq 0 ] && usage
 
-while getopts "hf:" arg
-do
+while getopts "hf:" arg; do
     case $arg in
-        f)
-            file=${OPTARG}
-            ;;
+    f)
+        file=${OPTARG}
+        ;;
     esac
 done
 
-cat ${file} | while read line
-do
+cat ${file} | while read line; do
     # continue if line is a comment
-    { grep -qe '^#' <<< ${line} ;} && continue
-    
-    regcode=`echo ${line} | cut -d ',' -f1`
-    ipaddr=`echo ${line} | cut -d ',' -f2`
-    desc=`echo ${line} | cut -d ',' -f3`
-    sn=`echo ${line} | cut -d ',' -f4`
-    
+    { grep -qe '^#' <<<${line}; } && continue
+
+    regcode=$(echo ${line} | cut -d ',' -f1)
+    ipaddr=$(echo ${line} | cut -d ',' -f2)
+    desc=$(echo ${line} | cut -d ',' -f3)
+    sn=$(echo ${line} | cut -d ',' -f4)
+
     echo "*** Registration code [${regcode}], IP address [${ipaddr}], description=[${desc}], SN [${sn}]"
-    ./ftnt-register-asset.py --code "${regcode}" --description "${desc}" --address "${ipaddr}" --serial "${sn}" --lic --verbose
+    python3 ftnt-register-asset.py --code "${regcode}" --description "${desc}" --address "${ipaddr}" --serial "${sn}" --lic --verbose
 done
